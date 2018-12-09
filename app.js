b62e90134a7bc45f8a0e49b5840aed8779f83b78
Anna Tomaszkiewicz <tomaszkiewicz.ania@gmail.com>
2018-12-09 10:38:48 +0100

    fresh start

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-Vue.use(vuelidate.default)
-
-const pizzaOrBurger = value => value === 'pizza' || value === 'burger' || !validators.helpers.req(value)
-const oldEnoughAndAlive = validators.between(12, 120)
-
 new Vue({
   el: '#app',
 
@@ -18,53 +13,12 @@ new Vue({
       }
     }
   },
-
-  validations: {
-    form: {
-      name: {
-        required: validators.required
-      },
-
-      age: {
-        required: validators.required, // $v.form.age.required
-        integer: validators.integer, // $v.form.age.integer
-        oldEnoughAndAlive // $v.form.age.oldEnoughAndAlive
-      },
-
-      email: {
-        email: validators.email,
-        required: validators.requiredIf(function () {
-          return !!this.form.newsletter
-        })
-      },
-
-      githubUsername: {
-        exists (value) {
-          if (!validators.helpers.req(value)) {
-            return true
-          }
-          return axios.get(`//api.github.com/users/${value}`)
-        }
-      },
-
-      food: {
-        pizzaOrBurger
-      }
-    }
-  },
-
   methods: {
-    shouldAppendValidClass (field) { // ex: field = $v.form.email
-      return !field.$invalid && field.$model && field.$dirty
-    },
-
-    shouldAppendErrorClass (field) { // ex: field = $v.form.email
-      return field.$error
-    },
-
     submitForm () {
-      this.$v.form.$touch()
-      if (!this.$v.form.$invalid) {
+      const nameIsValid = !!this.form.name
+      const ageIsValid = typeof this.form.age === 'number' && this.form.age>12 && this.form.age <120
+      const formIsValid = nameIsValid && ageIsValid
+      if (formIsValid) {
         console.log('📝 Form Submitted', this.form)
       } else {
         console.log('❌ Invalid form')
